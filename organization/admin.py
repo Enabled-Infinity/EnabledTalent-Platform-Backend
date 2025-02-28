8b9b2cf28c37ed83e089b119e8835298ef7536f8
CPTCIC5 <aryanjainak@gmail.com>
2025-02-28 11:15:20 +0530

    XYZ

--- organization/admin.py
+++ organization/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, OrganizationInvite
 # Register your models here.
 
 admin.site.register(Organization)
+admin.site.register(OrganizationInvite)
