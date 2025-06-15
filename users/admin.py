0c319a8debe0aa443658e34a56d6c614649fae68
CPTCIC5 <aryanjainak@gmail.com>
2025-06-15 10:11:13 +0530

    XYZ

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User,Profile, EmailVerificationToken
-# Register your models here.
+from .models import User,Profile, EmailVerificationToken, Feedback
+
+
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(EmailVerificationToken)
+admin.site.register(Feedback)
