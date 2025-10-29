dfc98513be0f615137d775418e2c16a8191e541d
CPTCIC5 <aryanjainak@gmail.com>
2025-10-29 14:44:58 +0530

    XYZ

--- backends/settings.py
+++ backends/settings.py
@@ -198,14 +198,6 @@ REST_FRAMEWORK = {
         ),
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 20,
-        'DEFAULT_RENDERER_CLASSES': [
-            'rest_framework.renderers.JSONRenderer',
-        ],
-        'DEFAULT_PARSER_CLASSES': [
-            'rest_framework.parsers.JSONParser',
-            'rest_framework.parsers.MultiPartParser',
-            'rest_framework.parsers.FormParser',
-        ],
     }
 
 CORS_ALLOW_HEADERS = [
